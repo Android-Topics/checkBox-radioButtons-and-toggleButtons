70fdcc2d68a2a17fecd13d024bcf58ef3f5b2acb
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-18 04:20:06 -0700

    committed

---
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    private CheckBox newsCheckBox;
+    private CheckBox termsCheckBox;
+    private RadioButton maleBtn;
+    private RadioButton femaleBtn;
+    private ToggleButton statusToggleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroupID);
+        newsCheckBox = findViewById(R.id.newsCheckBoxID);
+        termsCheckBox = findViewById(R.id.termsCheckBoxID);
+        maleBtn = findViewById(R.id.maleID);
+        femaleBtn = findViewById(R.id.femaleID);
+        statusToggleBtn = findViewById(R.id.statusID);
+
+
+    }
+
+    // checking a checkbox is checked or not in Java codes
+    public void checkBoxChecked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()){
+            case R.id.newsCheckBoxID:
+                Toast.makeText(this, "You will be receiving news updates", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.termsCheckBoxID:
+                Toast.makeText(this, "You just agreed to our terms and conditions", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
